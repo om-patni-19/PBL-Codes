389cd995c3e32dd6e3890c72d9603158a8ab9114
Om Manojkumar Patni <ompatni190805@gmail.com>
2025-04-23 12:06:45 +0530

    Update pbl7.cpp

--- pbl7.cpp
+++ pbl7.cpp
@@ -10,72 +10,77 @@ Description : N-Queen Problem
 #include <cstdlib>
 using namespace std;
 
-class NQueens {
-    int* x;
-    int n;
-    int count;
+class NQueens
+{
 
 public:
-    void solve(int size);
-    void Nqueens(int k);
-    bool place(int k, int i);
-    void display();
-    ~NQueens();
+     int x[10];
+     int n;
+     void NQueen(int k, int n);
+     bool Place(int k, int i);
+     void printBoard();
 };
 
+void NQueens::NQueen(int k, int n)
+{
+     if (k > n)
+     {
+          printBoard();
+          return;
+     }
 
-int main() {
-    int n;
-    cout << "Enter number of queens: ";
-    cin >> n;
+     for (int i = 1; i <= n; i++)
+     {
+          if (Place(k, i))
+          {
+               x[k] = i;
+               NQueen(k + 1, n);
 
-    NQueens q;
-    q.solve(n);
 
-    return 0;
+          }
+     }
+
 }
 
-void NQueens::solve(int size) {
-    n = size;
-    count = 1;
-    x = new int[n + 1];
-    Nqueens(1);
+bool NQueens::Place(int k, int i)
+{
+     for (int j = 1; j < k; j++)
+     {
+          if (x[j] == i || abs(x[j] - i) == abs(j - k))
+          {
+               return false;
+          }
+     }
+     return true;
 }
 
-
-void NQueens::Nqueens(int k) {
-    for (int i = 1; i <= n; i++) {
-        if (place(k, i)) {
-            x[k] = i;
-            if (k == n)
-                display();
-            else
-                Nqueens(k + 1);
-        }
-    }
+void NQueens::printBoard()
+{
+      int solutionCount = 1;
+    cout << "\nSolution " << solutionCount++ << ":\n";
+     for (int i = 1; i <= n; i++)
+     {
+          for (int j = 1; j <= n; j++)
+          {
+               if (x[i] == j)
+               {
+                    cout << "Q" << j;
+               }
+               else
+               {
+                    cout << ". ";
+               }
+          }
+          cout << endl;
+     }
+     cout << endl;
 }
 
-bool NQueens::place(int k, int i) {
-    for (int j = 1; j < k; j++) {
-        if (x[j] == i || abs(x[j] - i) == abs(j - k))
-            return false;
-    }
-    return true;
-}
-
-void NQueens::display() {
-    cout << "\nSolution " << count++ << ":\n";
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (x[i] == j)
-                cout << " Q ";
-            else
-                cout << " - ";
-        }
-        cout << "\n";
-    }
-}
-
-NQueens::~NQueens() {
-    delete[] x;
+int main()
+{
+     NQueens nq;
+     cout << "Enter number of queens: ";
+     cin >> nq.n;
+     nq.NQueen(1, nq.n);
+     return 0;
 }
